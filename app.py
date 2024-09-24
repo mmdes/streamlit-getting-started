6a9654150862010a4cd1bfd57b237cef31c1ff5a
Matheus Matos <57081161+mmdes@users.noreply.github.com>
2024-09-24 13:53:21 -0400

    Just doing some tests

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
 
-st.dataframe(dataframe.style.highlight_max(axis=0))
+st.map(map_data)
