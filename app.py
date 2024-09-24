86595c6a5982f781d0998aef6f58eaa431fa4615
Matheus Matos <57081161+mmdes@users.noreply.github.com>
2024-09-24 16:31:15 -0400

    some other tests

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+pre = left_column.button('Press me!')
 
-st.map(map_data)
+if pre:
+    print('o botão foi pressionado com sucesso!')
+
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
